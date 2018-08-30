7e1181a8fabaa678dd75c579be9eac656a91516a
farik88 <farik88@ua.fm>
2018-08-30 16:24:22 +0300

    Separate admin/frontend main.js script. Add dropzone plugin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -71,7 +71,7 @@
 
 <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
 <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/js/main.js') }}"></script>
+<script src="{{ asset('assets/admin/js/main.js') }}"></script>
 
 </body>
 </html>
